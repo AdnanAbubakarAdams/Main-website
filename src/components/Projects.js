8a657a3e1b82502d68840cce89f32433d38f6a8a
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-12-29 00:46:00 -0500

    still a work in progress

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,47 +1,123 @@
-import React from 'react'
+import React from "react";
+// import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
-    <div name="projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Projects</p>
-            <p className='py-6'>Check out some of my projets here</p>
+    <div
+      name="projects"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my projets here</p>
         </div>
-       
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-            <div className='shadow-md shadow-gray-600 rounded-lg'>
-                <img src="images/AddySports.png" alt="" className='rounded-md duration-200 hover:scale-105'/>
-                <div className='flex items-center justify-between'>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
-                </div>
+
+        <div className="grid sm:grid-cols-2 gap-8 px-12 sm:px-0">
+          <a href="https://addysports.netlify.app">
+            <div className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src="images/AddySports.png"
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-between">
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  React
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Javascript
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  CSS
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Express
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  PSQL
+                </button>
+              </div>
             </div>
-            <div className='shadow-md shadow-gray-600 rounded-lg'>
-                <img src="images/GoldenSolutions.png" alt="" className='rounded-md duration-200 hover:scale-105'/>
-                <div className='flex items-center justify-between'>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
-                </div>
+          </a>
+          <div className="shadow-md shadow-gray-600 rounded-lg">
+            <img
+              src="images/GoldenSolutions.png"
+              alt=""
+              className="rounded-md duration-200 hover:scale-105"
+            />
+            <div className="flex items-center justify-between">
+            <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  React
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Javascript
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  CSS
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Express
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  PSQL
+                </button>
             </div>
-            <div className='shadow-md shadow-gray-600 rounded-lg'>
-                <img src="images/volunteer.png" alt="" className='rounded-md duration-200 hover:scale-105'/>
-                <div className='flex items-center justify-center'>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
-                </div>
+          </div>
+          <div className="shadow-md shadow-gray-600 rounded-lg">
+            <img
+              src="images/volunteer.png"
+              alt=""
+              className="rounded-md duration-200 hover:scale-105"
+            />
+            <div className="flex items-center justify-center">
+            <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  React
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Javascript
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  CSS
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Express
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  PSQL
+                </button>
             </div>
-            <div className='shadow-md shadow-gray-600 rounded-lg'>
-                <img src="images/volunteer.png" alt="" className='rounded-md duration-200 hover:scale-105'/>
-                <div className='flex items-center justify-center'>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
-                </div>
+          </div>
+          <div className="shadow-md shadow-gray-600 rounded-lg">
+            <img
+              src="images/volunteer.png"
+              alt=""
+              className="rounded-md duration-200 hover:scale-105"
+            />
+            <div className="flex items-center justify-center">
+            <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  React
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Javascript
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  CSS
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  Express
+                </button>
+                <button className="w-1/2 px-2 py-3 duration-200 text-gray-500 hover:scale-125 hover:text-gray-300">
+                  PSQL
+                </button>
             </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
