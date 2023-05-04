1e07fcdd4ffed23b18c8b32fd7b0c14831b7a64e
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-05-04 15:18:48 -0400

    adding contact details to website

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
+// import { BsFillPersonLinesFill } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 
 const Social = () => {
@@ -8,7 +8,7 @@ const Social = () => {
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
     <ul>
         <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 '>
-            <a href='https://twitter.com' className='flex justify-between items-center w-full rounded-tr-md text-white'><>Twitter <FaTwitter size={30}/></> </a>
+            <a href='https://twitter.com/Energy_Gad_' className='flex justify-between items-center w-full rounded-tr-md text-white'><>Twitter <FaTwitter size={30}/></> </a>
         </li>
         <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 '>
             <a href='https://www.linkedin.com/in/adnan-abubakar-adams/' className='flex justify-between items-center w-full text-white'><>LinkedIn <FaLinkedin size={30}/></> </a>
@@ -19,9 +19,9 @@ const Social = () => {
         <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 '>
             <a href='mailto:abubakaradnanadams@pursuit.org' className='flex justify-between items-center w-full text-white'><>Mail <HiOutlineMail size={30}/></> </a>
         </li>
-        <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 '>
+        {/* <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 '>
             <a href='/resume.pdf' className='flex justify-between items-center w-full rounded-tr-md text-white'><>Resume <BsFillPersonLinesFill size={30}/></> </a>
-        </li>
+        </li> */}
     </ul>
     </div>
   )
